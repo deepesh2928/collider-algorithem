eb06d07f1f45ee38dda57075b46d1524d099b887
Deepesh <69512636+deepesh2928@users.noreply.github.com>
2020-08-18 17:00:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,28 @@
-var movingRect, fixedRect;
-
-
+var fixedRect, movingRect;
+var gameObject1,gameObject2;
 
 function setup() {
   createCanvas(1200,800);
-  movingRect =createSprite(400, 200, 80, 30);
-  movingRect.shapeColor='green';
-  movingRect.debug=true;
-  fixedRect = createSprite(600,400,50,80);
- fixedRect.shapeColor='green';
- fixedRect.debug=true;
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  gameObject1 = createSprite(368,579,20,70);
+  gameObject2 = createSprite(437,468,70,20);
 }
-
 function draw() {
-  background(255,255,255); 
-  movingRect.x=World.mouseX;
-  movingRect.y=World.mouseY;
-  if(movingRect.x-fixedRect.x < movingRect.width/2+fixedRect.width/2 &&
-    fixedRect.x-movingRect.x < movingRect.width/2+fixedRect.width/2 &&
-    movingRect.y-fixedRect.y < movingRect.height/2+fixedRect.height/2 &&
-    fixedRect.y-movingRect.y < movingRect.height/2+fixedRect.height/2){
-    console.log(movingRect.x-fixedRect.x)
-    movingRect.shapeColor='red';
-    fixedRect.shapeColor='red';
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  if(isTouching(movingRect,gameObject2)){
+    movingRect.shapeColor = "red";
+    gameObject2.shapeColor = "red";
+  }else {
+    movingRect.shapeColor = "green";
+    gameObject2.shapeColor = "green";
   }
-  else{
-    movingRect.shapeColor='green';
-    fixedRect.shapeColor='green';
-  }
-
-  
   drawSprites();
 }
